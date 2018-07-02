c4e7910a00d531f515c6dbebd9ef64a5b620fb7c
Echo7 <echo7@tiscali.it>
2018-07-02 17:45:24 +0200

    target priority 1

---
+++ 2.3_Update/ship/tai_missiledestroyer/singleplayer/props/default.lua
@@ -0,0 +1,10 @@
+Number_Properties_Priority = 1.0	-- Default - on conflict higher is used...
+
+Number_Properties = {
+}
+
+String_Properties_Priority = 1.0
+
+String_Properties = {
+	CanAttackFams = "Corvette_hw1, Corvette, Fighter_hw1, Fighter, Utility,,,,,, Frigate, SmallCapitalShip, BigCapitalShip, Mothership",	
+}
