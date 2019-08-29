807b4a1931fe89e3cb7370d9ea144ec0b71fcb0a
Cloaked <cloaked2222@outlook.com>
2019-08-29 16:58:10 -0400

    A few Balance Changes after Talking w/ Fear
    Corrected the max capital ship unit caps on normal.
    MD 4k range, less than ion cannon range.
    Tested and confirmed the cruisers use the carrier inhib module.
    Reverted cruiser health/speed changes. Not needed, cruiser is only 2x health of a DD atm.
    MD to 4k range per Fear.

---
+++ 2.3/scripts/races/kushan/deathmatch/unitcaps/normal.lua
@@ -0,0 +1,8 @@
+--Kushan
+supplyLimit("Frigate", 24);
+supplyLimit("Capital", 10);
+supplyLimit("Carrier", 3);
+
+supplyDesc("Battlecruiser", "HeavyCruiser");
+
+
---
+++ 2.3/scripts/races/taiidan/deathmatch/unitcaps/normal.lua
@@ -0,0 +1,6 @@
+--Taiidan
+supplyLimit("Frigate", 24);
+supplyLimit("Capital", 10);
+supplyLimit("Carrier", 3);
+
+supplyDesc("Battlecruiser", "HeavyCruiser");
