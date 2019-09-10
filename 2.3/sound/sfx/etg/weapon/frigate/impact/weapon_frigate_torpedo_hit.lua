88cb66d07ba7108e0fc54a31e0abdaef5bc61fe2
Novaras <tfear091@gmail.com>
2019-09-10 06:38:43 +0100

    meaty

---
+++ 2.3/sound/sfx/etg/weapon/frigate/impact/weapon_frigate_torpedo_hit.lua
@@ -0,0 +1,97 @@
+-- This file is generated automatically by the Relic Audio Tool
+
+version = 4
+
+volume = 0.600000
+frequency = -1
+maxPolyphony = 8
+envelope = {
+  {
+    distance = 0.000000,
+    volume = 0.440000,
+    reverb = 0.000000,
+    duration = 0,
+    equalizer = {
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+    }
+  },
+  {
+    distance = 966.666687,
+    volume = 0.335000,
+    reverb = 0.000000,
+    duration = 0,
+    equalizer = {
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+    }
+  },
+  {
+    distance = 2833.333252,
+    volume = 0.125000,
+    reverb = 0.000000,
+    duration = 0,
+    equalizer = {
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+    }
+  },
+  {
+    distance = 4000.000000,
+    volume = 0.10000,
+    reverb = 0.000000,
+    duration = 0,
+    equalizer = {
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+              0.870000,
+              0.760000,
+              0.790000,
+              0.740000,
+    }
+  },
+  {
+    distance = 6583.333496,
+    volume = 0.000000,
+    reverb = 0.000000,
+    duration = 0,
+    equalizer = {
+              1.000000,
+              1.000000,
+              1.000000,
+              1.000000,
+              0.250000,
+              0.170000,
+              0.000000,
+              0.000000,
+    }
+  },
+}
+container = 1
+playlist = 0
+randContainer = 1
+loopingPlaylist = 0
+silenceBreak = 0.000000
+silenceRandom = 0.000000
+randSampContainer = 1
+
