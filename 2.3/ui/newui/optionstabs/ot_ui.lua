792a21935232f342821014e6f766696d7ad7d31d
Cloaked <cloaked2222@outlook.com>
2019-10-26 23:53:20 -0400

    Stance Buttons Order Option
    Added an option to change the Stance Buttons Order, to match hw1c. Figured out how to optionaly load different UI screens via the uisettings.lua file!
    Increased the size of the main menu and in-game options menus to accomidate more options.
    Fixed the "Options>Change Settings>Cancel>Options>Accept scenario" for all the UI options (been bugged since b10).

--- 2.3/ui/newui/optionstabs/ot_ui.lua
+++ 2.3/ui/newui/optionstabs/ot_ui.lua
@@ -3,10 +3,16 @@ dofilepath("data:ui/newui/Styles/HWRM_Style/ControlConstructors.lua")
 
 --Load expanded options
 --dofilepath("data:ui/PlayersPatch_UI_Util.lua") --already loaded in scope by ot_gameplay.lua
+RefStanceOrderSetting = GetStanceOrderSetting()
+RefTacticalOverlaySetting = GetTacticalOverlaySetting()
+RefProductionTimeSetting = GetProductionTimeSetting()
 RefBuildResearchLaunchPanelsScale = GetBuildResearchLaunchPanelsScale()
 
--- Resets the additional slider positions
--- This properly handles the Options>Change Sliders>Cancel>Options>Accept scenario
+--Resets the expanded options
+--This properly handles the Options>Change Settings>Cancel>Options>Accept scenario
+UI_AnchorSetScale('PlayersPatch_StanceOrderSetting', RefStanceOrderSetting, 1);
+UI_AnchorSetScale('PlayersPatch_TacticalOverlaySetting', RefTacticalOverlaySetting, 1);
+UI_AnchorSetScale('PlayersPatch_ProductionTimeSetting', RefProductionTimeSetting, 1);
 UI_AnchorSetScale('PlayersPatch_BuildResearchLaunchPanelsScale', RefBuildResearchLaunchPanelsScale, 1);
 
 
@@ -27,7 +33,6 @@ FE_UI_UTILITYICONS_HUDSCALE	=	Option_LabeledSlider("UI_UTILITYICONS_HUDSCALE","$
 FE_UI_BOTTOMMIDDLE_HUDSCALE	=	Option_LabeledSlider("UI_BOTTOMMIDDLE_HUDSCALE","$5395",	"lblBottomMiddleHudScale",		"$5395",	"m_sbarBottomMiddleHUDScale",		"$5395", 1, 1, 0.5, 2.5, [[UI_AnchorSetScale('Taskbar_BottomMiddle_Scale', %spos, 1);]])
 FE_UI_RESOURCE_HUDSCALE		=	Option_LabeledSlider("UI_RESOURCE_HUDSCALE",	"$4984",	"lblResourceHudScale",			"$4984",	"m_sbarResourceHUDScale",			"$4984", 1, 1, 0.5, 2.5, [[UI_AnchorSetScale('Taskbar_ResourceHud_Scale', %spos, 1);]])
 
-------- Additional Sliders
 --Build, Research, & Launch Panels
 FE_UI_BRL_HUDSCALE				=	Option_LabeledSlider("UI_BRL_HUDSCALE",	"Build, Research, and Launch Panels",	"lblBRLHudScale",			"Build, Research, and Launch Panels",	"m_sbarBRLHUDScale",			"Adjust Build, Research, and Launch Panels. Default is 1. Must restart game to take effect.", 1, 1, 0.5, 1.5, [[UI_AnchorSetScale('PlayersPatch_BuildResearchLaunchPanelsScale', %spos, 1);]])
 
@@ -157,6 +162,87 @@ FE_UI_ATISKIN = {
 	},
 }
 
+FE_UI_STANCEORDER = {
+	type = "Frame",
+
+	visible = 1,
+	ignored = 1,
+	
+	Layout = {									
+		size_WH = {w = 1, h = 1, wr = "par", hr = "px",},
+		pad_LT = { l = 4, t = 0, lr = "px", tr = "px" },
+		pad_RB = { r = 4, b = -3, rr = "px", br = "px" },
+	},
+	
+	autosize = 1,
+	
+	helpTipTextLabel = "m_lblHelpText",
+	arrangetype = "horiz",
+	;
+	
+	{
+		type = "TextLabel",
+		
+		Layout = {	
+			pos_XY = { x = 1.0, y = .5, xr = "px", yr = "par" },		
+			size_WH = {	w = .45, h = DROPDOWN_HEIGHT, wr = "par", hr = "scr"},	
+			pivot_XY = { 0, 0.5 },	
+		},
+
+		Text = {
+			textStyle = "RM_GenericLabelLarge_TextStyle",
+			text = "Stance Buttons Order", 
+		},
+		autosize=1,
+	},
+	{
+		type = "DropDownListBox",	
+		name = "m_showtime",
+		selected = RefStanceOrderSetting - 1,
+		
+		Layout = {		
+			size_WH = {w = .5, h = DROPDOWN_HEIGHT, wr = "par", hr = "scr",},
+			pos_XY = { x = 1.0, y = .5, xr = "px", yr = "par" },		
+			pivot_XY = { 0, 0.5 },	
+		
+			--size_WH = {	w = 330/800, h = DROPDOWN_HEIGHT, wr = "scr", hr = "scr" },	
+		},
+		
+		helpTipTextLabel = "helpTip",
+		helpTip = "Change the order of the Stance Buttons. Default is Homeworld Remastered. Must relaunch Homeworld to take effect.",
+		
+		ListBox = {
+			type = "ListBox",
+			width = 400,
+			listBoxStyle = "FEListBoxStyle_Bordered",
+			backgroundColor = {0,0,0,255},
+			;
+			-- 1
+			{
+				type = "TextListBoxItem",
+				buttonStyle = "FEListBoxItemButtonStyle",
+				resizeToListBox = 1,
+				Text = {
+					textStyle = "FEListBoxItemTextStyle",
+					text = "$4803", --Homeworld Remastered
+				},
+				onMouseClicked = "UI_AnchorSetScale('PlayersPatch_StanceOrderSetting', 1.0, 1)";
+			},
+			-- 2
+			{
+				type = "TextListBoxItem",
+				buttonStyle = "FEListBoxItemButtonStyle",
+				resizeToListBox = 1,
+				Text = {
+					textStyle = "FEListBoxItemTextStyle",
+					text = "$3063", --Homeworld Classic
+				},	
+				onMouseClicked = "UI_AnchorSetScale('PlayersPatch_StanceOrderSetting', 2.0, 1)";
+			},
+		},
+	},
+}
+
 FE_UI_TACTICAL = {
 	type = "Frame",
 
@@ -193,7 +279,7 @@ FE_UI_TACTICAL = {
 	{
 		type = "DropDownListBox",	
 		name = "m_tactical",
-		selected = GetTacticalOverlaySetting() - 1,
+		selected = RefTacticalOverlaySetting - 1,
 		
 		Layout = {		
 			size_WH = {w = .5, h = DROPDOWN_HEIGHT, wr = "par", hr = "scr",},
@@ -296,7 +382,7 @@ FE_UI_SHOWPRODTIME = {
 	{
 		type = "DropDownListBox",	
 		name = "m_showtime",
-		selected = GetProductionTimeSetting() - 1,
+		selected = RefProductionTimeSetting - 1,
 		
 		Layout = {		
 			size_WH = {w = .5, h = DROPDOWN_HEIGHT, wr = "par", hr = "scr",},
@@ -307,7 +393,7 @@ FE_UI_SHOWPRODTIME = {
 		},
 		
 		helpTipTextLabel = "helpTip",
-		helpTip = "Shows the base build/research production times. Default is No. Must relaunch Homeworld to take effect.",
+		helpTip = "Show the Base Build/Research Production Times. Default is No. Must restart game to take effect.",
 		
 		ListBox = {
 			type = "ListBox",
@@ -322,7 +408,7 @@ FE_UI_SHOWPRODTIME = {
 				resizeToListBox = 1,
 				Text = {
 					textStyle = "FEListBoxItemTextStyle",
-					text = "$3183", --No
+					text = "$3222", --No
 				},
 				onMouseClicked = "UI_AnchorSetScale('PlayersPatch_ProductionTimeSetting', 1.0, 1)";
 			},
@@ -372,8 +458,9 @@ FE_UIOPTION_TAB = {
 						
 		-- OPTION FRAMES
 		
-		FE_UI_CURSORSKIN, --#1 in list
+		FE_UI_CURSORSKIN, --#1 in frame
 		FE_UI_ATISKIN,
+		FE_UI_STANCEORDER,
 		FE_UI_TACTICAL,
 		FE_UI_SHOWPRODTIME,
 
@@ -407,8 +494,7 @@ FE_UIOPTION_TAB = {
 		FE_UI_BOTTOMMIDDLE_HUDSCALE,
 		--FE_UI_RESOURCE_HUDSCALE,
 		
-		-- Additional Sliders
-		FE_UI_BRL_HUDSCALE, --#12 in list
+		FE_UI_BRL_HUDSCALE, --#13 in frame
 
 		--FE_UISPACER,
 		FE_UI_SHOWTIMER,
@@ -422,6 +508,6 @@ FE_UIOPTION_TAB = {
 	},
 }
 
---Sets the initial value/position for the additional sliders. The number in the second brack must be it's location in the list.
-FE_UIOPTION_TAB[1][12][2][1].Text.text = RefBuildResearchLaunchPanelsScale --initial value in the label
-FE_UIOPTION_TAB[1][12][2][2].scrollPosition = RefBuildResearchLaunchPanelsScale --initial position for the scrollbar
+--Sets the initial value/position for the expanded slider options. The number in the second brack must be it's location in the frame above.
+FE_UIOPTION_TAB[1][13][2][1].Text.text = RefBuildResearchLaunchPanelsScale --initial value in the label
+FE_UIOPTION_TAB[1][13][2][2].scrollPosition = RefBuildResearchLaunchPanelsScale --initial position for the scrollbar
