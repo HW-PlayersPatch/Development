c26e46e6711c644220a1072e3f0e53694cfd61fc
Cloaked <cloaked2222@outlook.com>
2019-12-09 00:37:03 -0500

    Cleaned up Observer Scripts
    Deleted a lot of un-neccessary lines copied over from Taiidan to the Observer race.

--- 2.3/scripts/races/observer/scripts/research/explicit_grant.lua
+++ 2.3/scripts/races/observer/scripts/research/explicit_grant.lua
@@ -1,55 +1,6 @@
 base_research = nil 
 base_research = {
 
-	{
-		Name =			"SGMCapitalHealth",
-		RequiredResearch =	"RESEARCH HACK",                        
-		RequiredSubSystems =	"",                              
-		Cost = 			0,
-		Time = 			0,
-		DisplayedName =		"",
-		DisplayPriority =	1208,                                                   
-		Description =		"",
-		UpgradeType =		Modifier,
-		TargetType =		Family,
-		TargetName =		"Capital",
-	        UpgradeName =		"MAXHEALTH",
-	        UpgradeValue =		0.5,
-		DoNotGrant = 1,
-	},
-	{
-		Name =			"SGMFrigateHealth",
-		RequiredResearch =	"RESEARCH HACK",                        
-		RequiredSubSystems =	"",                              
-		Cost = 			0,
-		Time = 			0,
-		DisplayedName =		"",
-		DisplayPriority =	1208,                                                   
-		Description =		"",
-		UpgradeType =		Modifier,
-		TargetType =		Family,
-		TargetName =		"Frigate",
-	        UpgradeName =		"MAXHEALTH",
-	        UpgradeValue =		0.45,
-		DoNotGrant = 1,
-	},
-	{
-		Name =			"SGMGravWellHealth",
-		RequiredResearch =	"RESEARCH HACK",                        
-		RequiredSubSystems =	"",                              
-		Cost = 			0,
-		Time = 			0,
-		DisplayedName =		"",
-		DisplayPriority =	1208,                                                   
-		Description =		"",
-		UpgradeType =		Modifier,
-		TargetType =		Ship,
-		TargetName =		"Tai_GravWellGenerator",
-		UpgradeName =		"MAXHEALTH",
-		UpgradeValue =		0.2,
-		DoNotGrant = 1,
-	},
-
 }
 
 -- Add these items to the research tree!
