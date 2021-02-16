966624e5d7ac99aa320676b0b7cea7af9680aacd
Novaras <tfear091@gmail.com>
2021-02-16 15:53:51 +0000

    comment

--- 2.3/scripts/playerspatch/ships_util/fighters.lua
+++ 2.3/scripts/playerspatch/ships_util/fighters.lua
@@ -1,3 +1,5 @@
+-- Start HW1 Scouts:
+
 function Scout_SpeedBurst_Start(CustomGroup)
 	SobGroup_SetMaxSpeedMultiplier(CustomGroup, 1.1)
 	FX_PlayEffect("speed_burst_flash", CustomGroup, 1.5)
@@ -12,3 +14,5 @@ end
 function Scout_SpeedBurst_Finish(CustomGroup)
 	SobGroup_SetMaxSpeedMultiplier(CustomGroup, 1)
 end
+
+-- end HW1 Scouts
