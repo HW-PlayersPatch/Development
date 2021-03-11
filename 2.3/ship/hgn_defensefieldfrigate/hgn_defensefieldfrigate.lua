d1d3df75823192915e43c36116174f5d1c11d838
Novaras <tfear091@gmail.com>
2021-03-11 15:44:14 +0000

    call fn on update

--- 2.3/ship/hgn_defensefieldfrigate/hgn_defensefieldfrigate.lua
+++ 2.3/ship/hgn_defensefieldfrigate/hgn_defensefieldfrigate.lua
@@ -1,3 +1,4 @@
 function Update_Hgn_DefenseFieldFrigate(CustomGroup, playerIndex, shipID)
     SobGroup_NoSalvageScuttle(CustomGroup, playerIndex, shipID)
+    DefenseFieldFrigate_Glow(CustomGroup, playerIndex, shipID)
 end
