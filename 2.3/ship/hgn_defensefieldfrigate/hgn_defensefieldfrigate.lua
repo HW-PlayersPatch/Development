50ddc55fda750bb157adf9d72d8ffd5b5470d4ff
Novaras <tfear091@gmail.com>
2021-07-12 05:05:25 +0100

    fix bug where dff could get stuck slow

--- 2.3/ship/hgn_defensefieldfrigate/hgn_defensefieldfrigate.lua
+++ 2.3/ship/hgn_defensefieldfrigate/hgn_defensefieldfrigate.lua
@@ -4,5 +4,7 @@ function Update_Hgn_DefenseFieldFrigate(CustomGroup, playerIndex, shipID)
 
     if (SobGroup_IsDoingAbility(CustomGroup, AB_DefenseField) == 1) then
         SobGroup_SetMaxSpeedMultiplier(CustomGroup, 0.7);
+    else
+        SobGroup_SetMaxSpeedMultiplier(CustomGroup, 1);
     end
 end
