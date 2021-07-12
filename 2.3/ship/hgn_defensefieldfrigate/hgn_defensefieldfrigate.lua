afd1a11189bfc2bcf8b47c456f7bfab2f961813c
Novaras <tfear091@gmail.com>
2021-07-12 04:54:27 +0100

    remove print

--- 2.3/ship/hgn_defensefieldfrigate/hgn_defensefieldfrigate.lua
+++ 2.3/ship/hgn_defensefieldfrigate/hgn_defensefieldfrigate.lua
@@ -3,7 +3,6 @@ function Update_Hgn_DefenseFieldFrigate(CustomGroup, playerIndex, shipID)
     DefenseFieldFrigate_Glow(CustomGroup, playerIndex, shipID);
 
     if (SobGroup_IsDoingAbility(CustomGroup, AB_DefenseField) == 1) then
-        print("nice");
         SobGroup_SetMaxSpeedMultiplier(CustomGroup, 0.7);
     end
 end
