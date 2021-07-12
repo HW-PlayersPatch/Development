43f09eb53c73e66bf6db1ba7c0903d14b00f58b5
Novaras <tfear091@gmail.com>
2021-07-12 01:54:41 +0100

    nerf def fields (25s active, 35s reacharge, slow speed)

--- 2.3/ship/hgn_defensefieldfrigate/hgn_defensefieldfrigate.lua
+++ 2.3/ship/hgn_defensefieldfrigate/hgn_defensefieldfrigate.lua
@@ -1,4 +1,9 @@
 function Update_Hgn_DefenseFieldFrigate(CustomGroup, playerIndex, shipID)
-    SobGroup_NoSalvageScuttle(CustomGroup, playerIndex, shipID)
-    DefenseFieldFrigate_Glow(CustomGroup, playerIndex, shipID)
+    SobGroup_NoSalvageScuttle(CustomGroup, playerIndex, shipID);
+    DefenseFieldFrigate_Glow(CustomGroup, playerIndex, shipID);
+
+    if (SobGroup_IsDoingAbility(CustomGroup, AB_DefenseField) == 1) then
+        print("nice");
+        SobGroup_SetMaxSpeedMultiplier(CustomGroup, 0.7);
+    end
 end
